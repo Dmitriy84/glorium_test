d8b2aac5719835cfc17fe5bc4283973410f4ceee
dmytromalohlovets <dmytro.malohlovets@i.ua>
2017-03-07 13:12:54 +0200

    added generic

--- UnitTestProject1/Steps/MyBookingPalComSteps.cs
+++ UnitTestProject1/Steps/MyBookingPalComSteps.cs
@@ -1,5 +1,4 @@
 ﻿using OlxUaTests.Model;
-using OlxUaTests.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,38 +10,46 @@ namespace OlxUaTests.Steps
 {
     class MyBookingPalComSteps
     {
+        private string _url;
+
+        public MyBookingPalComSteps(string url = "https://www.mybookingpal.com")
+        {
+            _url = url;
+        }
+
         public string GetLocationId(string[] address = null)
         {
-            var response = GetResponse("https://www.mybookingpal.com/api/location/getlocations/?term=" + address[0]);
-            var locations = new JavaScriptSerializer().Deserialize<List<MyBookingPalLocations>>(response);
+            var locations = GetResponseDeserialized<List<MyBookingPalLocations>>(_url + "/api/location/getlocations/?term=" + address[0]);
             var label = string.Join(", ", address);
             return locations.Find(i => i.label == label).ID;
         }
 
         public Quote GetProduct(string productId, string product, DateTime from, DateTime to)
         {
-            var response = GetResponse("https://www.mybookingpal.com/xml/services/json/reservation/products/" + productId + "/" + from.ToString("yyyy-MM-dd") + "/" + to.ToString("yyyy-MM-dd") + "?pos=a502d2c65c2f75d3&guests=2&amenity=true&currency=USD&exec_match=false&display_inquire_only=false");
-            var products = new JavaScriptSerializer().Deserialize<Products>(response);
+            var products = GetResponseDeserialized<Products>(_url + "/xml/services/json/reservation/products/" + productId + "/" + from.ToString("yyyy-MM-dd") + "/" + to.ToString("yyyy-MM-dd") + "?pos=a502d2c65c2f75d3&guests=2&amenity=true&currency=USD&exec_match=false&display_inquire_only=false");
             return products.search_response.search_quotes.quote.Find(p => p.productname == product);
         }
 
         public Prices GetPrices(int productid, DateTime date)
         {
-            var response = GetResponse("https://www.mybookingpal.com/xml/services/json/reservation/prices?pos=a502d2c65c2f75d3&productid=" + productid + "&fromdate=" + date + "&currency=USD");
-            return new JavaScriptSerializer().Deserialize<Prices>(response);
+            var response = GetResponseDeserialized<Prices>(_url + "/xml/services/json/reservation/prices?pos=a502d2c65c2f75d3&productid=" + productid + "&fromdate=" + date + "&currency=USD");
+            return response;
         }
 
-        private static string GetResponse(string url)
+        private static T GetResponseDeserialized<T>(string url)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            var request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "GET";
             request.ContentType = "application/json";
 
-            WebResponse webResponse = request.GetResponse();
-            using (Stream webStream = webResponse.GetResponseStream())
+            var webResponse = request.GetResponse();
+            using (var webStream = webResponse.GetResponseStream())
                 if (webStream != null)
-                    using (StreamReader responseReader = new StreamReader(webStream))
-                        return responseReader.ReadToEnd();
+                    using (var responseReader = new StreamReader(webStream))
+                    {
+                        var response = responseReader.ReadToEnd();
+                        return new JavaScriptSerializer().Deserialize<T>(response);
+                    }
                 else
                     throw new Exception("Response code: " + ((HttpWebResponse)webResponse).StatusCode);
         }
